c95c89bd5236f3889f14a65814c9503d5340be23
alexmartoos4-rgb <alexmartoos4@gmail.com>
2026-06-11 18:39:27 +0000

    First Commit

---
+++ src/js/index.js
@@ -0,0 +1,66 @@
+import "../style/index.scss";
+
+
+function render(variables = {}) {
+  console.log("These are the current variables: ", variables);
+  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+
+ 
+  document.querySelector("#widget_content").innerHTML = `<div class="widget">
+                ${cover}
+              <img src="${variables.avatarURL}" class="photo" />
+              <h1>${variables.name || "Lucy"} ${variables.lastName || "Boilett"}</h1>
+              <h2>${variables.role || ""}</h2>
+              <h3>${variables.city || ""}, ${variables.country || ""}</h3>
+              <ul class=${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/${variables.twitter ? variables.twitter : "4geeksacademy"}" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github ? variables.github : "4geeksacademy"}" target="_blank"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin ? variables.linkedin : "school/4geeksacademy"}" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram ? variables.instagram : "4geeksacademy"}" target="_blank"><i class="fab fa-instagram"></i></a></li>
+          </ul>
+            </div>
+        `;
+}
+
+
+window.onload = function() {
+  window.variables = {
+   
+    includeCover: true,
+   
+    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    
+    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    
+    socialMediaPosition: "left",
+   
+    twitter: null,
+    github: null,
+    linkedin: null,
+    instagram: null,
+    name: null,
+    lastName: null,
+    role: null,
+    country: null,
+    city: null
+  };
+  render(window.variables); // render the card for the first time
+
+  document.querySelectorAll(".picker").forEach(function(elm) {
+    elm.addEventListener("change", function(e) {
+      // <- add a listener to every input
+      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
+      let values = {};
+      values[attribute] =
+        this.value == "" || this.value == "null"
+          ? null
+          : this.value == "true"
+          ? true
+          : this.value == "false"
+          ? false
+          : this.value;
+      render(Object.assign(window.variables, values)); // render again the card with new values
+    });
+  });
+};
